d26ff09db4033fe05dc15d7d162c784ca456315f
Indio1983 <mruniverso7@gmail.com>
2023-05-14 17:20:28 -0300

    Prueba de repo Front

---
+++ Proyecto_Integrador_Ruben/BACKEND/src/main/java/com/rocportfolio/roc/RocApplication.java
@@ -0,0 +1,13 @@
+package com.rocportfolio.roc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocApplication.class, args);
+	}
+
+}
---
+++ Proyecto_Integrador_Ruben/BACKEND/src/test/java/com/rocportfolio/roc/RocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocportfolio.roc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
