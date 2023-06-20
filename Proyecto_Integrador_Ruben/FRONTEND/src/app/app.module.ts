705e42475f522e3334b289978e33dd54f724b556
Indio1983 <mruniverso7@gmail.com>
2023-06-20 20:15:31 -0300

    actualizacion
    se logra conectar a la bbdd

--- Proyecto_Integrador_Ruben/FRONTEND/src/app/app.module.ts
+++ Proyecto_Integrador_Ruben/FRONTEND/src/app/app.module.ts
@@ -20,6 +20,7 @@ import{ HttpClient, HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,9 @@ import { FormsModule } from '@angular/forms';
     FormsModule,
 
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
